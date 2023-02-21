9df8dd767b2c7d246987424294464be86622384d
Nathan Hemingway <nhemingway@gmail.com>
2023-02-21 11:18:31 -0800

    port alacritty config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,38 @@
-local wezterm = require("wezterm")
+local w = require("wezterm")
+
 return {
-	font = wezterm.font("RobotoMono Nerd Font Mono", { weight = "Medium" }),
-	font_size = 16.0,
-	font_antialias = "Subpixel",
+	-- Font
+	font = w.font("RobotoMono Nerd Font", { weight = "Medium" }),
+	font_size = 15.0,
+	-- cell_width = 1,
+	line_height = 1.1,
+	--
 	color_scheme = "nord",
-	cell_width = "0.8",
+	window_decorations = "RESIZE",
+	debug_key_events = true,
+	window_padding = {
+		left = 8,
+		right = 8,
+		top = 4,
+		bottom = 4,
+	},
+	hide_tab_bar_if_only_one_tab = true,
+	window_frame = {
+		active_titlebar_bg = "#191c23",
+		inactive_titlebar_bg = "#191c23",
+	},
+	colors = {
+		tab_bar = {
+			active_tab = {
+				bg_color = "#2e3440",
+				fg_color = "#d8dee9",
+			},
+			inactive_tab = {
+				bg_color = "#222730",
+				fg_color = "#616e88",
+			},
+		},
+	},
+	scrollback_lines = 3500,
+	keys = require("keys"),
 }
